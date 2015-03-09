22b58b3622af7553c9a7bcf39145d9e3f0a509d8
Tushar Sharma <rcsharma@Tushar-Sharmas-MacBook-Pro.local>
2015-03-10 00:13:33 +0530

    scroll js made async

--- js/scale.fix.js
+++ js/scale.fix.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 var metas = document.getElementsByTagName('meta');
 var i;
 if (navigator.userAgent.match(/iPhone/i)) {
@@ -17,14 +19,4 @@ function gestureStart() {
   }
 }
 
-/*
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-39341257-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-*/
+});
