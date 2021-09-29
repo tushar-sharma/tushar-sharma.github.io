e280404bda281f2c85432a8ab8a59ce74db916f7
Tushar Sharma <>
2021-09-29 02:10:55 -0400

    update script

--- scripts/updateHabits.py
+++ scripts/updateHabits.py
@@ -2,6 +2,7 @@
 import gspread
 import time
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, timedelta
 
 def main():
     """update google sheet"""
@@ -51,6 +52,19 @@ def main():
     for sheet_counter in range(lower_row, upper_row + 1):
         print("Deleting value for O%s" % (sheet_counter))
         sheet_instance.update("O%s" % (sheet_counter), "" , value_input_option='USER_ENTERED')
+
+    time.sleep(15)
+
+    # update the sheet date
+    date_col = "S"
+
+    date_col_row_number = 2
+
+    curr_val = sheet_instance.acell("%s%s"%(date_col,date_col_row_number)).value
+
+    res = (datetime.strptime(curr_val, '%a %b %d %Y') + timedelta(days=1)).strftime('%a %b %d %Y')
+
+    sheet_instance.update("%s%s" % (date_col, date_col_row_number), res , value_input_option='USER_ENTERED')
 
 
 if __name__=="__main__":
