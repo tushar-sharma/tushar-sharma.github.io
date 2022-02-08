38bcdb89d3869a695161083116f9c93130b496af
Tushar Sharma <>
2022-02-08 16:08:27 -0500

    added dark mode

--- js/dark_wrapper.js
+++ js/dark_wrapper.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-    document.getElementsByClassName('DarkReader_Button')[0].onclick = function() { 
-        darkmode()  
+    document.getElementsByClassName('DarkReader_Button')[0].onclick = function() {
+        darkmode()
     }
-    
+
     let enabled = localStorage.getItem('dark-mode')
 
     if (enabled === null) {
@@ -13,24 +13,24 @@ $(document).ready(function(){
     } else if (enabled === 'true') {
       enable()
     }
-    
+
     function enable()  {
       DarkReader.setFetchMethod(window.fetch)
       DarkReader.enable();
       localStorage.setItem('dark-mode', 'true');
-      document.getElementById('dark-text').innerText = "Lightmode";
+      document.getElementById('dark-text').innerText = "Sunrise";
       document.getElementById('icon-dark').className = "fa fa-sun";
     }
     function disable() {
       DarkReader.disable();
       localStorage.setItem('dark-mode', 'false');
-      document.getElementById('dark-text').innerText = "Darkmode";
+      document.getElementById('dark-text').innerText = "Sunset";
       document.getElementById('icon-dark').className = "fa fa-moon";
     }
     function darkmode() {
-      if (localStorage.getItem('dark-mode') === 'false') { 
-          enable(); 
-      } else { 
+      if (localStorage.getItem('dark-mode') === 'false') {
+          enable();
+      } else {
           disable();
       }
     }
