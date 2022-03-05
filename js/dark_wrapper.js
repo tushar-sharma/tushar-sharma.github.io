25057560b426b53f2c6d45561bbe94098f5bb7e3
Tushar Sharma <>
2022-03-05 04:23:13 -0500

    added sound

--- js/dark_wrapper.js
+++ js/dark_wrapper.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     }
 
     let enabled = localStorage.getItem('dark-mode')
+    let audio = document.createElement('audio');
 
     if (enabled === null) {
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -19,11 +20,15 @@ $(document).ready(function(){
       DarkReader.enable();
       localStorage.setItem('dark-mode', 'true');
       document.getElementById('icon-dark').className = "fa fa-sun";
+      audio.src = "../sounds/static_switch-on.mp3";
+      audio.play();
     }
     function disable() {
       DarkReader.disable();
       localStorage.setItem('dark-mode', 'false');
       document.getElementById('icon-dark').className = "fa fa-cloud-moon";
+      audio.src = "../sounds/static_switch-off.mp3";
+      audio.play();
     }
     function darkmode() {
       if (localStorage.getItem('dark-mode') === 'false') {
