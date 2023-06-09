4d3c8cd3e5d11e014247d60f0f1862bbf5bf86aa
Tushar Sharma <tushar.sharma1729@gmail.com>
2023-06-09 00:21:49 -0400

    updated code clipboard

--- js/copyClipboard.js
+++ js/copyClipboard.js
@@ -2,22 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
   var copyButtons = document.querySelectorAll('.copy-button');
 
   copyButtons.forEach(function(button) {
-    var codeBlock = button.previousElementSibling.querySelector('code');
-    var clipboard = new ClipboardJS(button, {
-      text: function() {
-        return codeBlock.textContent;
-      }
-    });
+      var codeBlock = button.previousElementSibling;
+      var content = codeBlock.innerText;
 
-    clipboard.on('success', function() {
-      button.textContent = 'Copied!';
-      setTimeout(function() {
-        button.textContent = 'Copy';
-      }, 2000);
-    });
+      button.addEventListener('click', function() {
+          var tempTextarea = document.createElement('textarea');
+          tempTextarea.value = content;
 
-    clipboard.on('error', function() {
-      button.textContent = 'Press Ctrl+C to copy';
-    });
+          document.body.appendChild(tempTextarea);
+          tempTextarea.select();
+          document.execCommand('copy');
+          document.body.removeChild(tempTextarea);
+
+          button.textContent = 'Copied!';
+          setTimeout(function() {
+              button.textContent = 'Copy';
+          }, 2000);
+      });
   });
 });
