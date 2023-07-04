4fd70be94189cbe6def8bfd43a2457e060c7fd02
Tushar Sharma <tushar.sharma1729@gmail.com>
2023-07-04 14:24:44 -0400

    updated post

--- createPost.sh
+++ createPost.sh
@@ -11,7 +11,7 @@ postdate=`date "+%Y-%m-%d"`
 jekylldate=`date "+%Y-%m-%d"`
 read -p "Enter the file name: " file_variable;
 read -p "Enter the post name: " post_variable;
-read -p "Enter the image url from Unsplash: " image_variable;
+read -p "Enter the image name " image_variable;
 read -p "Enter the author name: " author_variable;
 touch _posts/$postdate-$file_variable.md
 cat <<EOF > _posts/$postdate-$file_variable.md
