9fadaa959386f53bf2d130f22442292dbcb7b116
Tushar Sharma <>
2025-03-03 10:29:04 -0500

    v1 pagination changes

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,7 +1,6 @@
 module Jekyll
-
   class TagIndex < Page    
-    def initialize(site, base, dir, tag)
+    def initialize(site, base, dir, tag, page_num, total_pages, posts)
       @site = site
       @base = base
       @dir = dir
@@ -10,7 +9,15 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
-      self.data['title'] = "Posts Tagged: "+tag
+      self.data['title'] = "Posts Tagged 333: #{tag}" + (page_num > 1 ? " (Page #{page_num})" : "")
+      self.data['posts'] = posts
+      self.data['paginator'] = {
+        'page'          => page_num,
+        'per_page'      => site.config['pagination']['per_page'],
+        'total_pages'   => total_pages,
+        'previous_page' => page_num > 1 ? page_num - 1 : nil,
+        'next_page'     => page_num < total_pages ? page_num + 1 : nil
+      }
     end
   end
 
@@ -18,20 +25,29 @@ module Jekyll
     safe true
     
     def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = 'tag'
-        site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
-        end
+      return unless site.layouts.key?('tag_index')
+      tag_base_dir = 'tag'
+      site.tags.each do |tag, posts|
+        paginate_tag(site, tag, posts, tag_base_dir)
+      end
+    end
+
+    def paginate_tag(site, tag, posts, tag_base_dir)
+      per_page = site.config['pagination']['per_page'].to_i
+      total_pages = (posts.size.to_f / per_page).ceil
+      (1..total_pages).each do |page_num|
+        paginated_posts = posts[((page_num - 1) * per_page), per_page]
+        # For page 1, URL: /tag/<tag>/ ; for other pages: /tag/<tag>/pageX/
+        dir = page_num == 1 ? File.join(tag_base_dir, tag) : File.join(tag_base_dir, tag, "page#{page_num}")
+        write_tag_index(site, dir, tag, page_num, total_pages, paginated_posts)
       end
     end
   
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
+    def write_tag_index(site, dir, tag, page_num, total_pages, posts)
+      index = TagIndex.new(site, site.source, dir, tag, page_num, total_pages, posts)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index
     end
   end
-
 end
