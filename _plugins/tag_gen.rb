1f0d20662894d22fce7ae4c9ca5efc89cd8f1c78
Tushar Sharma <>
2025-03-03 13:25:06 -0500

    fixed typo

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -9,7 +9,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
-      self.data['title'] = "Posts Tagged 333: #{tag}" + (page_num > 1 ? " (Page #{page_num})" : "")
+      self.data['title'] = "Posts Tagged: #{tag}" + (page_num > 1 ? " (Page #{page_num})" : "")
       self.data['posts'] = posts
       self.data['paginator'] = {
         'page'          => page_num,
