f4b29b4a30fc039e081ea3e6a9b9d04a13399c4f
Tushar Sharma <>
2025-09-05 20:15:34 -0400

    added post

--- createPost.sh
+++ createPost.sh
@@ -25,7 +25,9 @@ category: blog
 published: false
 ---
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<!-- truncate_here -->
+Dear Vishi, dear logs for today.<!-- truncate_here -->
+
+Dear Vishi, dear logs for today.
 EOF
 exit 0
 
